58ba4681d930c3889e56f5e87d7005d210d40a20
Kyle Housel <kylegranthousel@gmail.com>
2025-05-22 09:46:56 -0400

    Pass all tests, not exactly certain how I did it though

--- index.js
+++ index.js
@@ -9,6 +9,68 @@
  for you to use if you need it!
  */
 
+const createEmployeeRecord = (array) => {
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+
+const createEmployeeRecords = (array) => {
+  const employeeRecords = []
+
+  array.forEach(employee => employeeRecords.push(createEmployeeRecord(employee)))
+
+  return employeeRecords
+}
+
+const createTimeInEvent = function (dateStamp) {
+
+  const stamp = {
+    type: "TimeIn",
+    hour: Number(dateStamp.slice(11,15)),
+    date: dateStamp.slice(0, 10)
+  }
+
+  this.timeInEvents.push(stamp)
+
+  return this
+}
+
+const createTimeOutEvent = function (dateStamp) {
+
+  const stamp = {
+    type: "TimeOut",
+    hour: Number(dateStamp.slice(11,15)),
+    date: dateStamp.slice(0, 10)
+  }
+
+  this.timeOutEvents.push(stamp)
+
+  return this
+}
+
+const hoursWorkedOnDate = function (formDate) {
+  const timeIn = this.timeInEvents.find(e => e.date === formDate)
+  const timeOut = this.timeOutEvents.find(e => e.date === formDate)
+
+  if (!timeIn || !timeOut) {
+    throw new Error(`Missing timeIn or timeOut for date ${formDate}`);
+  }
+
+  return (timeOut.hour - timeIn.hour) / 100
+}
+
+const wagesEarnedOnDate = function (formDate) {
+  const hoursWorked = hoursWorkedOnDate.call(this, formDate)
+
+  return hoursWorked * this.payPerHour
+}
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -21,3 +83,12 @@ const allWagesFor = function () {
     return payable
 }
 
+const findEmployeeByFirstName = function (employeeRecords, firstName) {
+    return employeeRecords.find(employee => employee.firstName === firstName)
+}
+
+const calculatePayroll = function (employeeRecords) {
+
+  return employeeRecords.reduce((acc, employee) => acc += allWagesFor.call(employee), 0)
+
+}
